6874e8379d9752b94779c2a4a07a45e0a203d539
LARIBI Younes <12306158@r20509.iutv.univ-paris13.fr>
2026-06-09 16:39:42 +0200

    Fix: Nettoyage forcé du fichier de test

--- src/test/java/com/example/demo/service/StatistiqueImplTest.java
+++ src/test/java/com/example/demo/service/StatistiqueImplTest.java
@@ -1,28 +1,36 @@
 package com.example.demo.service;
 
 import com.example.demo.data.Voiture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
 
 public class StatistiqueTests {
 
-    public static void main(Strings[] args){
-    
-        List<Voiture> voitures = new ArrayList<Voiture>();
-        Voiture voiture1= new Voiture("Ferrarri", 2);
-        Voiture voiture2= new Voiture("Lambo", 2);
-        Voiture voiture3= new Voiture("Rolls Royce", 2);
+    private StatistiqueImpl statistique;
 
-        @Test
-        voitures.ajouter(voiture1);
-        voitures.ajouter(voiture2);
-        voitures.ajouter(voiture3);
-        Assertions.assertEquals(voitures.prixMoyen(),2);
+    @BeforeEach
+    void setUp() {
+        statistique = new StatistiqueImpl();
+    }
+
+    @Test
+    void testPrixMoyenNormal() throws ArithmeticException {
+        statistique.ajouter(new Voiture("Ferrari", 100));
+        statistique.ajouter(new Voiture("Renault", 200));
+
+        Echantillon resultat = statistique.prixMoyen();
+
+        assertEquals(2, resultat.getNombreDeVoitures());
+        assertEquals(150, resultat.getPrixMoyen());
+    }
+
+    @Test
+    void testPrixMoyenSansVoiture() {
+        assertThrows(ArithmeticException.class, () -> {
+            statistique.prixMoyen();
+        });
     }
 }
 
