4110b9ac9c735e21fef64ebe05c23477672979e0
LARIBI Younes <12306158@r20509.iutv.univ-paris13.fr>
2026-06-09 15:14:46 +0200

    Ajout des tests unitaires complets pour la classe Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -6,12 +6,25 @@ import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+package packmain;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 public class VoitureTest {
+    
+    private Voiture tuture;
+    
+    @BeforeEach
+    void setUp(){
+        tuture = new Voiture("Ferrari", 200000);
+    }
+    
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void testGetters(){
+        Assertions.assertEquals(200000, tuture.getPrix(), "Le prix doit être 200000");
+        Assertions.assertEquals("Ferrari", tuture.getMarque(), "La marque doit être Ferrari");
     }
-
 }
